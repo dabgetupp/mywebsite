b368a12834f24c8ca41e3332a0abe3016026c2ce
dabgetup <prastowoteguhw@gmail.com>
2021-08-15 17:41:55 +0700

    page about

--- about/views.py
+++ about/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'title':'About',
+        'heading':'About',
+        'subheading':'Tentang Prastowo Teguh W',
+    }
+
+    return render(request, 'about/index.html', context)
